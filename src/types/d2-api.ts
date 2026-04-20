2b07edba2440e2ddf9685569bfd9b46c4e66a547
Arnau Sanchez <pyarnau@gmail.com>
2026-04-20 11:25:58 +0200

    infra: use explicit exports

--- src/types/d2-api.ts
+++ src/types/d2-api.ts
@@ -1,1 +1,22 @@
-export * from "@eyeseetea/d2-api/2.42";
+export { D2Api } from "@eyeseetea/d2-api/2.42";
+
+export type {
+    D2CategoryCombo,
+    D2CategoryComboSchema,
+    D2CategoryOptionCombo,
+    D2CategoryOptionGroup,
+    D2DataElement,
+    D2DataElementGroup,
+    D2DataSet,
+    D2Indicator,
+    D2Section,
+    D2User,
+    D2ValidationRule,
+    DataValueSetsDataValue,
+    DataValueSetsPostRequest,
+    MetadataPick,
+    MetadataResponse,
+    PartialPersistedModel,
+    Ref,
+    SelectedPick,
+} from "@eyeseetea/d2-api/2.42";
